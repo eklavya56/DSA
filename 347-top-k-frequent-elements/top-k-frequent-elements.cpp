6df12f00d2946e7f82e073e223cb3eb1c8858489
eklavya56 <93969584+eklavya56@users.noreply.github.com>
2025-10-08 02:56:45 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (36.08%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <unordered_map>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) freq[num]++;
+
+        auto comp = [](pair<int,int>& a, pair<int,int>& b) { return a.second > b.second; };
+        priority_queue<pair<int,int>, vector<pair<int,int>>, decltype(comp)> minHeap(comp);
+
+        for (auto& entry : freq) {
+            minHeap.push(entry);
+            if (minHeap.size() > k) minHeap.pop();
+        }
+
+        vector<int> result;
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().first);
+            minHeap.pop();
+        }
+
+        return result;
+    }
+};
