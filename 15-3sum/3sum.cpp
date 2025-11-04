b5f95065308447d9363fc128a59f18cdb15c9908
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2025-11-05 00:58:48 +0530

    Time: 47 ms (69.85%) | Memory: 29.1 MB (48.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates
+            if (i > 0 && nums[i] == nums[i - 1]) continue; 
             int l = i + 1, r = nums.size() - 1;
 
             while (l < r) {
