6b952fb8b39be97c95e646fb0650def40007ddce
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-02-15 02:12:27 +0530

    Time: 6 ms (13.67%) | Memory: 11.8 MB (25.72%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int ans = 0;
+
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '('){
+                st.push(i);
+            }
+            else{
+                st.pop();
+                
+                if(st.empty()){
+                    st.push(i);
+                }
+                else{
+                    int len = i - st.top();
+                    if(len > ans){
+                        ans = len;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
