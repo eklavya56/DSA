26cc8edacaee266c5a86617f42831eab8a8388ed
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-04-06 03:53:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.89%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int n = moves.length();
+        int x = 0, y = 0;
+
+        for(int i = 0; i < n; i++){
+            if(moves[i] == 'U') y++;
+            else if(moves[i] == 'D') y--;
+            else if(moves[i] == 'L') x--;
+            else if(moves[i] == 'R') x++;
+        }
+
+        if(x == 0 && y == 0) return true;
+        return false;
+    }
+};
