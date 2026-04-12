fce872dd194809413f88add30bc5f821236df215
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-04-13 02:25:33 +0530

    Time: 3 ms (10.72%) | Memory: 8.3 MB (46.44%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -3,17 +3,11 @@ public:
     bool canBeEqual(string s1, string s2) {
         
         if (s1 == s2) return true;
-
-        // swap (0,2)
         swap(s1[0], s1[2]);
         if (s1 == s2) return true;
-
-        // revert and try (1,3)
         swap(s1[0], s1[2]);
         swap(s1[1], s1[3]);
         if (s1 == s2) return true;
-
-        // try both swaps
         swap(s1[0], s1[2]);
         if (s1 == s2) return true;
 
