ff3f510bdb4b10fa44d2a517d1e379d4e1f8a62b
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-05-09 03:12:28 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (91.75%) - LeetSync

--- 168-excel-sheet-column-title/excel-sheet-column-title.cpp
+++ 168-excel-sheet-column-title/excel-sheet-column-title.cpp
@@ -3,10 +3,9 @@ public:
     string convertToTitle(int col) {
         string ans;
         while(col>0){
-            col--;
-            int r=col%26;
+            int r=(col-1)%26;
             ans.push_back(r + 'A');
-            col=col/26;
+            col=(col-1)/26;
         }
         reverse(ans.begin(),ans.end());
         return ans;
