4b6a21d4370f2f800f4d62d2567bb1255352782f
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-05-10 04:14:02 +0530

    Time: 16 ms (45.99%) | Memory: 17.8 MB (51.82%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.cpp
@@ -3,22 +3,28 @@ public:
     vector<vector<int>> rotateGrid(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        int layers=min(m,n)/2;
+        int layers=min(m,n)/2;//each layer require two rows and two columns
         for(int layer=0;layer<layers;layer++){
             vector<int>nums;
-            int top=layer;
+            int top=layer; 
             int bottom=m-layer-1;
             int right=n-layer-1;
             int left=layer;
+             // Top row (left -> right)
             for(int i=left;i<=right;i++){
                 nums.push_back(grid[top][i]);
             }
+            // Right column (top -> bottom)
+            // Skip corners because they are already included in top/bottom rows
             for(int j=top+1;j<=bottom-1;j++){
                 nums.push_back(grid[j][right]);
             }
+             // Bottom row (right -> left)
             for(int i=right;i>=left;i--){
                 nums.push_back(grid[bottom][i]);
             }
+             // Left column (bottom -> top)
+            // Skip corners because they are already included in top/bottom rows
             for(int j=bottom-1;j>=top+1;j--){
                 nums.push_back(grid[j][left]);
             }
