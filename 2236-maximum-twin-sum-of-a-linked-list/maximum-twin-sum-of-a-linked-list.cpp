7073d72b36142a8db7174894300b83388bb54833
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-06-14 23:57:10 +0530

    Time: 11 ms (13.25%) | Memory: 120.6 MB (96.43%) - LeetSync

--- 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
+++ 2236-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,43 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    int pairSum(ListNode* head) {
 
-        // Find middle
+    ListNode* middleNode(ListNode* head){
         ListNode* slow = head;
         ListNode* fast = head;
 
-        while(fast && fast->next)
-        {
+        while(fast->next && fast->next->next){
             slow = slow->next;
             fast = fast->next->next;
         }
 
-        // Reverse second half
-        ListNode* prev = NULL;
-        ListNode* curr = slow;
+        return slow;
+    }
 
-        while(curr)
-        {
-            ListNode* nextNode = curr->next;
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            ListNode* temp = curr->next;
             curr->next = prev;
             prev = curr;
-            curr = nextNode;
+            curr = temp;
         }
+        return prev;
+    }
 
-        // Find maximum twin sum
-        int maxi = 0;
-
-        ListNode* first = head;
-        ListNode* second = prev;
-
-        while(second)
-        {
-            maxi = max(maxi, first->val + second->val);
-
+    int pairSum(ListNode* head) {
+        ListNode* middle = middleNode(head);
+        ListNode* second = middle->next;
+        middle->next = NULL;
+        ListNode* first = reverse(head);
+        int res = 0;
+        while(first){
+            res = max(res,first->val + second->val);
             first = first->next;
             second = second->next;
         }
-
-        return maxi;
+        return res;
     }
 };
