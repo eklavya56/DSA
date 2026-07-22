fcd280a722f616b97ecab86567e79e4c0cbaa387
Eklavya Verma <93969584+eklavya56@users.noreply.github.com>
2026-07-23 02:33:11 +0530

    Time: 43 ms (5.01%) | Memory: 45.7 MB (5.17%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -6,25 +6,17 @@ public:
 
         while (k--) {
             vector<vector<int>> temp = grid;
-
-            // Move every element one step to the right
             for (int i = 0; i < m; i++) {
                 for (int j = 0; j < n - 1; j++) {
                     temp[i][j + 1] = grid[i][j];
                 }
             }
-
-            // Move last element of each row to first of next row
             for (int i = 0; i < m - 1; i++) {
                 temp[i + 1][0] = grid[i][n - 1];
             }
-
-            // Move last element of grid to first position
             temp[0][0] = grid[m - 1][n - 1];
-
             grid = temp;
         }
-
         return grid;
     }
 };
